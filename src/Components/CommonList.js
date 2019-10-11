cc69f04607f89f2249c989894f221ccb8dce0f98
indork <56440509+indork@users.noreply.github.com>
2019-10-11 19:47:50 +0700

    Update CommonList.js

--- src/Components/CommonList.js
+++ src/Components/CommonList.js
@@ -34,11 +34,6 @@ export default class CommonList extends React.Component {
                   </h5>
                   <span className={mergeClassNames(BulmaCSS['level-right'])}>{rightSide}</span>
                 </div>
-                <h6
-                  className={mergeClassNames(BulmaCSS.subtitle, Styles.companyTitle, BulmaCSS['is-size-6'])}
-                >
-                  {authorityWebSite ? <a href={getFixedUrl(authorityWebSite)} target='_blank' >{authority}</a> : authority}
-                </h6>
                 { authorityMeta
                   ? <span className={mergeClassNames(Styles.companyMeta)}>{`(${authorityMeta})`}</span>
                   : null}
